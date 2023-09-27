346d4cd9ec93fccab0a3b2759cf3f801fd0958a1
Lucas Aires <116677841+lucasairesdev@users.noreply.github.com>
2023-09-27 15:16:07 -0300

    Animating items with Script and using DeltaTime to save frame value

---
+++ Assets/Project/Scripts/AnimationRotate.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationRotate : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
